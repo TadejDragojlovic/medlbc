c63b35dd8cfc547b558f899f0f5502925588ad61
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2025-12-15 15:56:06 +0100

    persistent read function for handling event triggered EPOLLINS

--- include/utils.h
+++ include/utils.h
@@ -3,9 +3,12 @@
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
 #include <stdarg.h> // za logiranje
 
 int set_nonblock(int sockfd);
 void logg(const char* format, ...);
+ssize_t persistent_read(int fd, void* buf, size_t count);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -27,3 +27,34 @@ void logg(const char* format, ...) {
 
     va_end(args);
 }
+
+/* persistent read function for event-triggered reading;
+   returns 0 if closed connection, -1 if error, otherwise total bytes read */
+ssize_t persistent_read(int fd, void* buf, size_t count) {
+    ssize_t total = 0;
+
+    for(;;) {
+        ssize_t nbytes = read(fd, buf+total, count-total);
+
+        if(nbytes > 0) {
+            total += nbytes;
+
+            if(total == count) break;
+            continue;
+        }
+
+        // connection closed by peer
+        if(nbytes == 0) {
+            return 0;
+        }
+
+        // read successfully
+        if(errno == EAGAIN || errno == EWOULDBLOCK) break;
+
+        // otherwise error
+        perror("read");
+        return -1;
+    }
+
+    return total;
+}
