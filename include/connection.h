336626af20a68ad092144648a085c5ba308f1a7c
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2025-12-15 19:45:08 +0100

    Added connection context and detailed FD structure

--- include/connection.h
+++ include/connection.h
@@ -5,6 +5,38 @@
 #include <sys/socket.h>
 #include <errno.h>
 
+#define REQ_BUF_SIZE 4096
+
+struct ConnectionContext {
+    int clientfd;
+    struct FDInfo* client_info;         // detailed fd info for the client
+
+    int upstreamfd;
+    struct FDInfo* upstream_info;       // detailed fd info for the upstream server
+
+    enum { 
+        CTX_IDLE,                       // client didn't send a request yet
+        CTX_CONNECTING_TO_UPSTREAM,
+        CTX_FORWARDING_REQUEST,
+        CTX_WAITING_RESPONSE,
+        CTX_SUCCESS,
+        CTX_ERROR,
+    } status;
+
+    char buf[REQ_BUF_SIZE];
+    size_t buflen;
+};
+
+enum FDType { FD_CLIENT, FD_UPSTREAM };
+
+struct FDInfo {
+    int fd;
+    enum FDType type;
+    struct ConnectionContext* ctx;
+};
+
+struct ConnectionContext* initialize_new_connection_context();
+struct FDInfo* initialize_new_fdinfo_structure(int fd, enum FDType type, struct ConnectionContext* ctx);
 void close_conn(WorkerProcess* worker, int conn_fd);
 int handle_new_conn(WorkerProcess* worker, int listenerfd);
 
--- src/connection.c
+++ src/connection.c
@@ -1,9 +1,37 @@
 #include "connection.h"
 
+// TODO: improve this function to work with FDInfo
 void close_conn(WorkerProcess* worker, int conn_fd) {
     close(conn_fd);
     worker->num_conn--;
     logg("Client %d [Worker: %d] hangup!", conn_fd, worker->pid);
+}
+
+/* Allocates memory for a new ConnectionContext and sets default values (including client_fd) */
+struct ConnectionContext* initialize_new_connection_context(int client_fd) {
+    struct ConnectionContext* ctx = malloc(sizeof(struct ConnectionContext));
+    if(!ctx) return NULL;
+    memset(ctx, 0, sizeof(*ctx));
+
+    ctx->clientfd = client_fd;
+    ctx->upstreamfd = -1;
+    ctx->status = CTX_IDLE;
+
+    return ctx;
+}
+
+/* Allocates memory for a new FDInfo structure;
+   It initializes values according to parameters, ctx can be NULLED */
+struct FDInfo* initialize_new_fdinfo_structure(int fd, enum FDType type, struct ConnectionContext* ctx) {
+    struct FDInfo* fi = malloc(sizeof(struct FDInfo));
+    if(!fi) return NULL;
+    memset(fi, 0, sizeof(*fi));
+
+    fi->fd = fd;
+    fi->type = type;
+    fi->ctx = ctx;
+
+    return fi;
 }
 
 /* Accepting all new queued up connections */
@@ -11,8 +39,6 @@ int handle_new_conn(WorkerProcess* worker, int listenerfd) {
     int clientfd;
     struct sockaddr_storage client_addr;
     socklen_t client_addrlen = sizeof(client_addr);
-
-    struct epoll_event event;
 
     for(;;) {
         clientfd = accept(listenerfd, (struct sockaddr*)&client_addr, &client_addrlen);
@@ -32,15 +58,28 @@ int handle_new_conn(WorkerProcess* worker, int listenerfd) {
             continue;
         }
 
-        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
-        event.data.fd = clientfd;
-        if(epoll_ctl(worker->efd, EPOLL_CTL_ADD, clientfd, &event) < 0) {
+        // connection context
+        struct ConnectionContext* ctx = initialize_new_connection_context(clientfd); // TODO: error handle
+
+        // info about the file descriptor
+        struct FDInfo* fdinfo = initialize_new_fdinfo_structure(clientfd, FD_CLIENT, ctx); // TODO: error handle
+
+        // fdinfo needs to be accessible from ctx
+        ctx->client_info = fdinfo;
+
+        struct epoll_event ev = {
+            .events = EPOLLIN | EPOLLET | EPOLLRDHUP,
+            .data.ptr = fdinfo,
+        };
+        if(epoll_ctl(worker->efd, EPOLL_CTL_ADD, clientfd, &ev) < 0) {
             perror("epoll_ctl");
+            // TODO: implement proper cleanup
             close(clientfd);
+            free(ctx);
+            free(fdinfo);
             continue;
         }
 
-        // printf("Uspesno prihvacena nova konekcija!\n");
         logg("New connection accepted!");
         worker->num_conn++;
     }
